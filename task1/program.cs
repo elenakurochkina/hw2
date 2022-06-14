dbb5a3d0511b24ae254b7c57157646b12b82b7fa
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-14 21:12:09 +0300

    task1 completed

---
+++ task1/program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.Write("введите трехзначное число: ");
+string x = Console.ReadLine();
+int a = int.Parse(x);
+int b = a % 100 / 10;
+if (a > 99 && a < 1000) System.Console.WriteLine($"{b}");
+if (a < 99 || a > 1000) System.Console.WriteLine("wrong number");  
+    
+       
