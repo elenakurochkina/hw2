5b37b6906af0690d8d21b9c77afa76e44124cc56
Elena Kurochkina <e.kuro4kina@gmail.com>
2022-06-15 12:10:36 +0300

    task4 completed

---
+++ task3/program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("enter number: ");
+string x = Console.ReadLine();
+int a = int.Parse(x);
+if (a < 100) System.Console.WriteLine("no third number");
+else
+while (a > 999)
+{
+    a = a / 10;
+}
+System.Console.WriteLine($"{a%10}");
